fa51723e8012d5157ea62600b80baea5408975aa
Vanessa Ma <ykvanessama@uchicago.edu>
2020-02-15 17:53:14 -0600

    running wrapper, integrated with User class

--- data_utils.py
+++ data_utils.py
@@ -8,3 +8,19 @@ class User():
         self.phone = phone
         self.email = email
         self.address = address
+
+class Dx_ICF():
+    def __init__(self, dx_icf = '')
+        self.icf = dx_icf
+
+class Dx_Other():
+    def __init__(self, dx_cat = '', dx_sub = '')
+        self.cat = dx_cat
+        self.sub = dx_sub
+
+class Treatment():
+    def __init__(self, treatment_type = '', treatment_name = '', treatment_rx_bool = '', treatment_rx = '')
+        self.type = treatment_type
+        self.name = treatment_name
+        self.rx_bool = treatment_rx_bool
+        self.rx = treatment_rx
--- find_your_disability_input.py
+++ find_your_disability_input.py
@@ -1,92 +1,114 @@
 import numpy as np 
 import pymongo as pm
+from data_utils import User, Diagnosis, Treatment
 
 def User_Build():
-    name = input('What is your name? (Last, First) \n')
-    age = input('What is your age? Use numerals please. \n')
+    fname = input('What is your first name? \n')
+    lname = input('What is your last name? \n')
+    dob = input('What is your date of birth? (ddmmyyyy). \n')
     gender = input('What is your gender? (Male / Female / Nonbinary) \n')
     phone = input('What is your preferred telephone number? No special characters, include numbers only and country code \n')
     email = input('What is your email? This will be used in the future to locate your records. \n')
     address = input('Where do you live? Feel free to input to any level of granularity, and to use your clinic or health provider\'s address if you would prefer. \n')
-    return(name, age, gender, phone, email, address)
+    return(fname, lname, dob, gender, phone, email, address)
 
 def Section_Redirect():
-    section = input('What information would you like to log? Input the relevant number. \n 1 - Diagnostic \n 2 - Treatment')
+    section = input('What information would you like to log? Input the relevant number. \n 1 - Diagnostic \n 2 - Treatment \n')
     return(section)
 
-def Diagnostic():
-    end_var = 'N'
-    dx_type = input('What diagnosis type would you like to input? \n (ICF / Other)')
-    if dx_type == 'ICF':
-        while end_var == 'N'
-            dx = input('What ICF code(s) were you diagnosed with? Input one. \n')
-            end_var = ('Would you like to input more ICF codes? Y/N: ')
-    else: 
-        
 
+###################################### ALL DIAGNOSTIC DATA COLLECTION ######################################
+def Diagnostic():
+    end_var = 'Y'
+    while end_var == 'Y':
+        dx_type = input('What diagnosis type would you like to input? \n (ICF / Other): ')
+        end_var = Diagnostic_Specific(dx_type)
+        
 dx_dict = {
-    1: ['Mental', 'Sensory and Pain', 'Voice and Speech', 'Cardiovascular, Haematological, Immunological and Respiratory', 'Digestive, Metabolic, Endocrine', 'Genitourinary, Reproductive', 'Neuromusculoskeletal, Movement-related', 'Skin'],
-    2: ['Learning, Applying Knowledge', 'General Tasks, Demands', 'Communication', 'Mobility', 'Self-Care', 'Domestic Life', 'Interpersonal Interactions, Relationships', 'Major Life Areas', 'Community, Social, Civic Life']
-    3: ['PRoducts and Technology', 'Natural Environment and Human-made Changes', 'Support and Relationships', 'Attitudes', 'Services, Systems, Societies']
-    4: ['Nervous System', 'Eye, Ear, and Related', 'Voice and Speech', 'Cardiovascular, Immunological, Respiratory, ']
+    '1': ['Mental', 'Sensory and Pain', 'Voice and Speech', 'Cardiovascular, Haematological, Immunological and Respiratory', 'Digestive, Metabolic, Endocrine', 'Genitourinary, Reproductive', 'Neuromusculoskeletal, Movement-related', 'Skin'],
+    '2': ['Learning, Applying Knowledge', 'General Tasks, Demands', 'Communication', 'Mobility', 'Self-Care', 'Domestic Life', 'Interpersonal Interactions, Relationships', 'Major Life Areas', 'Community, Social, Civic Life'],
+    '3': ['Products and Technology', 'Natural Environment and Human-made Changes', 'Support and Relationships', 'Attitudes', 'Services, Systems, Societies'],
+    '4': ['Nervous System', 'Eye, Ear, and Related', 'Voice and Speech', 'Cardiovascular, Immunological, Respiratory', 'Digestive, Metabolic, Endocrine', 'Genitourinary, Reproductive', 'Neuromusculoskeletal, Movement-related', 'Skin']
 }
 
-def Diagnostic_General():
-    dx_section = input('What section granularity would you like to provide? \n 1 - Body Functions \n 2 - Body Structures \n  3 - Activities and Participation \n 4 - Environmental Factors \n')
-    print('Your options are the following')
-    for a, b in enumerate(dx_dict[dx_section], 1):
-        print("{} {}.format (a, b)")
-    dx_desc_idx = input('Please input the number next to the section that most relates to your diagnosis: ')
-    dx_desc = dx_dict[dx_section][dx_desc - 1]
+def Diagnostic_Specific(dx_type):
+    end_var_branch = 'Y'
+    if dx_type == 'ICF':
+        while end_var_branch == 'Y':
+            dx = input('What ICF code(s) were you diagnosed with? Input one. \n')
+            end_var_branch = input('Would you like to input more ICF codes? Y/N: ')
+    else: 
+        while end_var_branch == 'Y':
+            dx_section = input('What section granularity would you like to provide? \n 1 - Body Functions \n 2 - Body Structures \n  3 - Activities and Participation \n 4 - Environmental Factors \n')
+            print('Your options are the following')
+            for a, b in enumerate(dx_dict[dx_section], 1):
+                print("{} {}".format (a, b))
+            dx_desc_idx = int(input('Please input the number next to the section that most relates to your diagnosis: '))
+            dx_desc = dx_dict[dx_section][dx_desc_idx - 1]
+            end_var_branch = input('Would you like to input any other non-ICF diagnostic information? (Y/N): ')
+    end_var = input('Would you like to input any other diagnostic information? (Y/N): ')
+    return(end_var)
 
+###################################### ALL TREATMENT DATA COLLECTION ######################################
 def Treatment():
-    treatment_type = input('What type of intervention did you receive? \n 1 - Medication \n 2 - Procedure \n 3 -Device) \n')
+    end_var = 'Y'
+    while end_var == 'Y':
+        treatment_type = input('What type of intervention did you receive? \n 1 - Medication \n 2 - Procedure \n 3 - Device \n')
+        end_var = Treatment_Specific(treatment_type)
+
+def Treatment_Specific(treatment_type):
+    end_var_branch = 'Y'
     # DRUG CONDITION
-    end_var = 'N'
-    if treatment_type == 1:
-        while end_var == 0
+    if treatment_type == '1':
+        while end_var_branch == 'Y':
             drug = input('Please input the name of one drug: ')
-            rx_drug_bool = ('Did you have an adverse reaction to the drug? (Y/N): ')
+            rx_drug_bool = input('Did you have an adverse reaction to the drug? (Y/N): ')
             if rx_drug_bool == 'Y':
                 rx_drug = input('Please input adverse reactions to the drug: ') 
-            end_var = input('Would you like to input another drug? (Y/N): ')
+            end_var_branch = input('Would you like to input another drug? (Y/N): ')
     # PROCEDURE CONDITION
-    elif treatment_type == 2:
-        while end_var == 0
+    elif treatment_type == '2':
+        while end_var_branch == 'Y':
             prd = input('Please input the name of a procedure: ')
             rx_prd_bool = input('Did you have an adverse reaction to the procedure? (Y/N): ')
             if rx_prd_bool == 'Y':
                 rx_prd = input('Please input adverse reactions to the procedure: ')
-            end_var = input('Would you like to input another procedure? (Y/N): ')
-    elif treatment_type == 3:
-        while end_var == 0
+            end_var_branch = input('Would you like to input another procedure? (Y/N): ')
+    # DEVICE CONDITION
+    elif treatment_type == '3':
+        while end_var_branch == 'Y':
             device = input('Please input the name of a device you were given: ')
             rx_device_bool = input('Did you have an adverse reaction to the device? (Y/N): ')
             if rx_device_bool == 'Y':
                 rx_device = input('Please input adverse reactions to the device: ')
-            end_var = input('Would you like to input another device? (Y/N): ')
+            end_var_branch = input('Would you like to input another device? (Y/N): ')
+    end_var = input('Would you like to input any other treatment information? (Y/N): ')
+    return(end_var)    
 
-
+###################################### WRAPPER FUNCTIONS ######################################
 def main():
     user_arb = input('Welcome to Find Your Disability. Do you have an account with us? (Y/N): ')
     if user_arb == 'N':
-        [name, age, gender, phone, email, address] = User_Build()
-    section = Section_Redirect()
-    # Diagnostic Inputs
-    if section == 1:
-        Diagnostic()
-
-
-
-
-
+        [fname, lname, dob, gender, phone, email, address] = User_Build()
+        new_user = User(fname, lname, dob, gender, phone, email, address)
+    end_var = 'Y'
+    while end_var == 'Y':
+        section = Section_Redirect()    
+        # Diagnostic Inputs
+        if section == '1':
+            Diagnostic()
+        if section == '2':
+            Treatment()
+        end_var = input('Would you like to input any other information today? Y/N: ')
+    if end_var == 'N':
+        print('Thank you. Have a great day!') 
 
 # New to System / Adding to Records
 
 def User_Locate():
     user_email = input('What is the email you used to register with us? \n')
     # Query the data base to get the user_id
-
+    user_id = 'Placebo'
     return(user_id)
 
 
