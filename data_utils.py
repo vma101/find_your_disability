4650ddf0caf7a239b55fed6bcd17aef8777f57c8
Andrew Wells <andrew.wells1024@gmail.com>
2020-02-15 18:06:15 -0600

    Fixing syntax

--- data_utils.py
+++ data_utils.py
@@ -10,16 +10,16 @@ class User():
         self.address = address
 
 class Dx_ICF():
-    def __init__(self, dx_icf = '')
+    def __init__(self, dx_icf = ''):
         self.icf = dx_icf
 
 class Dx_Other():
-    def __init__(self, dx_cat = '', dx_sub = '')
+    def __init__(self, dx_cat = '', dx_sub = ''):
         self.cat = dx_cat
         self.sub = dx_sub
 
 class Treatment():
-    def __init__(self, treatment_type = '', treatment_name = '', treatment_rx_bool = '', treatment_rx = '')
+    def __init__(self, treatment_type = '', treatment_name = '', treatment_rx_bool = '', treatment_rx = ''):
         self.type = treatment_type
         self.name = treatment_name
         self.rx_bool = treatment_rx_bool
