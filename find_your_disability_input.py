bce9fb3a6d79c7b6b63d97fd082d426b588cff30
Vanessa Ma <ykvanessama@uchicago.edu>
2020-02-16 10:51:36 -0600

    Merge!

--- find_your_disability_input.py
+++ find_your_disability_input.py
@@ -1,7 +1,9 @@
 from backend import SqliteDB
 from data_utils import *
+from geopy.geocoders import Nominatim
 
 db = SqliteDB("backend.db")
+
 
 def User_Build():
     fname = input('What is your first name? \n')
@@ -11,12 +13,14 @@ def User_Build():
     phone = input('What is your preferred telephone number? No special characters, include numbers only and country code \n')
     email = input('What is your email? This will be used in the future to locate your records. \n')
     address = input('Where do you live? Feel free to input to any level of granularity, and to use your clinic or health provider\'s address if you would prefer. \n')
+    geolocator = Nominatim(user_agent = 'python')
+    location_long = geolocator.geocode(address)
+    location = (location.latitude, location.longitude)
     return(fname, lname, dob, gender, phone, email, address)
 
 def Section_Redirect():
     section = input('What information would you like to log? Input the relevant number. \n 1 - Diagnostic \n 2 - Treatment \n')
     return(section)
-
 
 ###################################### ALL DIAGNOSTIC DATA COLLECTION ######################################
 def Diagnostic():
@@ -25,7 +29,7 @@ def Diagnostic():
         dx_type = input('What diagnosis type would you like to input? \n (ICF / Other): ')
         end_var = Diagnostic_Specific(dx_type)
         
-dx_dict = {
+dx_sub_dict = {
     '1': ['Mental', 'Sensory and Pain', 'Voice and Speech', 'Cardiovascular, Haematological, Immunological and Respiratory', 'Digestive, Metabolic, Endocrine', 'Genitourinary, Reproductive', 'Neuromusculoskeletal, Movement-related', 'Skin'],
     '2': ['Learning, Applying Knowledge', 'General Tasks, Demands', 'Communication', 'Mobility', 'Self-Care', 'Domestic Life', 'Interpersonal Interactions, Relationships', 'Major Life Areas', 'Community, Social, Civic Life'],
     '3': ['Products and Technology', 'Natural Environment and Human-made Changes', 'Support and Relationships', 'Attitudes', 'Services, Systems, Societies'],
@@ -42,11 +46,13 @@ def Diagnostic_Specific(dx_type):
         while end_var_branch == 'Y':
             dx_section = input('What section granularity would you like to provide? \n 1 - Body Functions \n 2 - Body Structures \n  3 - Activities and Participation \n 4 - Environmental Factors \n')
             print('Your options are the following')
-            for a, b in enumerate(dx_dict[dx_section], 1):
+            for a, b in enumerate(dx_sub_dict
+        [dx_section], 1):
                 print("{} {}".format (a, b))
             dx_desc_idx = int(input('Please input the number next to the section that most relates to your diagnosis: '))
-            dx_desc = dx_dict[dx_section][dx_desc_idx - 1]
-            end_var_branch = input('Would you like to input any other non-ICF diagnostic information? (Y/N): ')
+            dx_desc = dx_sub_dict
+        [dx_section][dx_desc_idx - 1]
+            end_var_branch = input('Would you like to input any other non-coded diagnostic information? (Y/N): ')
     end_var = input('Would you like to input any other diagnostic information? (Y/N): ')
     return(end_var)
 
@@ -62,26 +68,26 @@ def Treatment_Specific(treatment_type):
     # DRUG CONDITION
     if treatment_type == '1':
         while end_var_branch == 'Y':
-            drug = input('Please input the name of one drug: ')
+            drug = input('Please input the name of one drug (limit 1024char): ')
             rx_drug_bool = input('Did you have an adverse reaction to the drug? (Y/N): ')
             if rx_drug_bool == 'Y':
-                rx_drug = input('Please input adverse reactions to the drug: ') 
+                rx_drug = input('Please input adverse reactions to the drug (limit 1024char): ') 
             end_var_branch = input('Would you like to input another drug? (Y/N): ')
     # PROCEDURE CONDITION
     elif treatment_type == '2':
         while end_var_branch == 'Y':
-            prd = input('Please input the name of a procedure: ')
+            prd = input('Please input the name of a procedure (limit 1024char): ')
             rx_prd_bool = input('Did you have an adverse reaction to the procedure? (Y/N): ')
             if rx_prd_bool == 'Y':
-                rx_prd = input('Please input adverse reactions to the procedure: ')
+                rx_prd = input('Please input adverse reactions to the procedure (limit 1024char): ')
             end_var_branch = input('Would you like to input another procedure? (Y/N): ')
     # DEVICE CONDITION
     elif treatment_type == '3':
         while end_var_branch == 'Y':
-            device = input('Please input the name of a device you were given: ')
+            device = input('Please input the name of a device you were given (limit 1024char): ')
             rx_device_bool = input('Did you have an adverse reaction to the device? (Y/N): ')
             if rx_device_bool == 'Y':
-                rx_device = input('Please input adverse reactions to the device: ')
+                rx_device = input('Please input adverse reactions to the device (limit 1024char): ')
             end_var_branch = input('Would you like to input another device? (Y/N): ')
     end_var = input('Would you like to input any other treatment information? (Y/N): ')
     return(end_var)    
